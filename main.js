0f33a6c7e7c3ae97283ae7731a5eae38d63ea7cc
Alex N <77512968+AlexNelson52@users.noreply.github.com>
2024-03-31 19:00:20 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -31,18 +31,19 @@ const playGame = (() =>{
       board.splice(e.getAttribute('id'),1,marker);
     } else {
     if(marker === player1.marker){
-      h1Text.textContent = "Player 1's Turn"
+      h1Text.textContent = `${player1.name}'s Turn`
       marker = player2.marker
      e.textContent = player2.marker
      board.splice(e.getAttribute('id'),1,marker);
 
     } else
     if(marker === player2.marker){
-      h1Text.textContent = "Player 2's Turn"
+      h1Text.textContent = `${player2.name}'s Turn`
       marker = player1.marker
       e.textContent = player1.marker
       board.splice(e.getAttribute('id'),1,marker);
-    }
+    } 
+    
     return gameWinnerCondition(board)
   }
     }
@@ -53,64 +54,66 @@ const playGame = (() =>{
 function gameWinnerCondition(input){
 
     if(input[0] === 'X' && input[1] === 'X' && input[2] === 'X' && input[0,1,2] !== 'O' && input[0,1,2] !== ''){
-      h1Text.textContent = 'Player 1 Wins'
+      h1Text.textContent = `${player1.name} Wins`
      return true
 } else 
     if(input[1] === 'X' && input[4] === 'X' && input[7] === 'X' && input[1,4,7] !== 'O' && input[1,4,7] !== ''){
-      h1Text.textContent = 'Player 1 Wins'
+      h1Text.textContent = `${player1.name} Wins`
      return true
 } else 
     if(input[0] === 'X' && input[3] === 'X' && input[6] === 'X'  && input[0,3,6] !== 'O' && input[0,3,6] !== ''){
-      h1Text.textContent = 'Player 1 Wins'
+      h1Text.textContent = `${player1.name} Wins`
      return true
 } else 
     if(input[6] === 'X' && input[7] === 'X' && input[8] === 'X' && input[6,7,8] !== 'O' && input[6,7,8] !== ''){
-      h1Text.textContent = 'Player 1 Wins'
+      h1Text.textContent = `${player1.name} Wins`
      return true
 } else 
     if(input[3] === 'X' && input[4] === 'X' && input[5] === 'X' && input[3,4,5] !== 'O' && input[3,4,5] !== ''){
-      h1Text.textContent = 'Player 1 Wins'
+      h1Text.textContent = `${player1.name} Wins`
      return true
 } else 
     if(input[2] === 'X' && input[4] === 'X' && input[6] === 'X' && input[2,4,6] !== 'O' && input[2,4,6] !== ''){
-      h1Text.textContent = 'Player 1 Wins'
+      h1Text.textContent = `${player1.name} Wins`
      return true
 } else 
     if(input[0] === 'X' && input[4] === 'X' && input[8] === 'X'&& input[0,4,8] !== 'O' && input[0,4,8] !== ''){
-      h1Text.textContent = 'Player 1 Wins'
+      h1Text.textContent = `${player1.name} Wins`
       return true
 } else 
     if(input[0] === 'O' && input[1] === 'O' && input[2] === 'O'  && input[0,1,2] !== 'X' && input[0,1,2] !== ''){
-      h1Text.textContent = 'Player 2 Wins'
+      h1Text.textContent = `${player2.name} Wins`
      return true
 } else 
     if(input[1] === 'O' && input[4] === 'O' && input[7] === 'O' && input[1,4,7] !== 'X' && input[1,4,7] !== ''){
-      h1Text.textContent = 'Player 2 Wins'
+      h1Text.textContent = `${player2.name} Wins`
       return true
 } else 
     if(input[0] === 'O' && input[3] === 'O' && input[6] === 'O' && input[0,3,6] !== 'X' && input[0,3,6] !== ''){
-      h1Text.textContent = 'Player 2 Wins'
+      h1Text.textContent = `${player2.name} Wins`
       return true
 } else 
     if(input[6] === 'O' && input[7] === 'O' && input[8] === 'O' && input[6,7,8] !== 'X' && input[6,7,8] !== ''){
-      h1Text.textContent = 'Player 2 Wins'
+      h1Text.textContent = `${player2.name} Wins`
      return true
 } else 
     if(input[3] === 'O' && input[4] === 'O' && input[5] === 'O' && input[3,4,5] !== 'X' && input[3,4,5] !== ''){
-      h1Text.textContent = 'Player 2 Wins'
+      h1Text.textContent = `${player2.name} Wins`
      return true
 } else 
     if(input[2] === 'O' && input[4] === 'O' && input[6] === 'O'&& input[2,4,6] !== 'X' && input[2,4,6] !== ''){
+      h1Text.textContent = `${player2.name} Wins`
      return true
 }
   else 
     if(input[0] === 'O' && input[4] === 'O' && input[8] === 'O'&& input[0,4,8] !== 'X' && input[0,4,8] !== ''){
-      h1Text.textContent = 'Player 2 Wins'
+      h1Text.textContent = `${player2.name} Wins`
      return true
+  } else
+  if (input[0] !== '' && input[1] !== '' && input[2] !== '' && input[3] !== '' && input[4] !== '' && input[5] !== '' && input[6] !== '' && input[7] !== '' && input[8] !== '') {
+    h1Text.textContent = "DRAW!"};
   }
 
-
-}
 newGameBtn.addEventListener('click', () => {
 
   for(let i = 0; i < row.length; i++){
